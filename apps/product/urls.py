e1435ae1388fbc509074d71dc3f59421098c6419
Vignesh Karthik <mailboxkvk@gmail.com>
2026-04-10 17:38:41 +0530

    get_total_amount import removed

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_products, get_total_amount
+from .views import get_products
 
 urlpatterns = [
     path('get/', get_products, name='get-products'),
