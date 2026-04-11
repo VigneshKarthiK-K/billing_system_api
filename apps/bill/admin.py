26b12d3c943dcc7af5c96ab41b7d6efc811daada
Vignesh Karthik <mailboxkvk@gmail.com>
2026-04-11 10:16:14 +0530

    BillItem register in admin site

--- apps/bill/admin.py
+++ apps/bill/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bill
+from .models import Bill, BillItem
 
 # Register your models here.
 admin.site.register(Bill)
+admin.site.register(BillItem)
